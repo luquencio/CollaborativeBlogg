c95e5150b7f5cadd022d7fe01e5457df8f9ce789
Lucas Carpio <lucas.evans.carpio@gmail.com>
2014-06-29 14:00:01 -0400

    improving models

--- app/app2/models.py
+++ app/app2/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Catetoria(models.Model):
+	titulo = models.CharField(max_length = 200)
+
+class Enlace(models.Model):
+	titulo = models.CharField(max_length = 200)
+	enlace = models.URLField()
+	votos = models.IntegerField(default = 0) 
